802b7d4c026805a6928af25b3672a174d9aadcc4
Blake Sergesketter <blakesergesketter@gmail.com>
2023-03-29 20:13:56 -0500

    solve challenge 1

--- each.rb
+++ each.rb
@@ -11,3 +11,11 @@ weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
+
+
+weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+
+weather_options.each do |options|
+    puts "The forecast today is #{options}!"
+end 
+
